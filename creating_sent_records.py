f7e3654ba2b1f88829ad8169b38bece1a375044e
rrtucci <tucci@ar-tiste.com>
2024-02-26 18:13:46 -0500

    first commit

--- creating_sent_records.py
+++ creating_sent_records.py
@@ -3,6 +3,22 @@ import pandas as pd
 from globals import *
 
 
+def articulate_data(df, row, col_name):
+    entry = df.at[row, col_name]
+    time_diff = df.at[row, "DateTimeDiff"]
+    if str(entry).lower() == "nan":
+        str0 = ""
+    elif col_name in ["Id", "DateTime", "DateTimeDiff"]:
+        str0 = ""
+    elif col_name == "DayOfWeek":
+        str0 = f"Today is {entry} {ZTZ_SEPARATOR}"
+    elif "Diff" in col_name:
+        str0 = f"{col_name} is {entry} after {time_diff} {ZTZ_SEPARATOR}"
+    else:
+        str0 = f"{col_name} is {entry} {ZTZ_SEPARATOR}"
+    return str0
+
+
 def create_sent_records():
     in_dir = "patient_csv_records"
     out_dir = "patient_sent_records"
@@ -16,21 +32,7 @@ def create_sent_records():
             out_str = ""
             for row in range(len(df)):
                 for col_name in col_names:
-                    if col_name in ["Id", "DateTime", "DateTimeDiff"]:
-                        continue
-                    entry = df.at[row, col_name]
-                    time_diff = df.at[row, "DateTimeDiff"]
-                    if col_name == "DayOfWeek":
-                        out_str += f"Today is {entry} {ZTZ_SEPARATOR}"
-                        continue
-                    if "Diff" not in col_name:
-                        if entry:
-                            out_str += f"{col_name} is {entry}" \
-                                       f" {ZTZ_SEPARATOR}"
-                    else:
-                        if entry:
-                            out_str += f"{col_name} is {entry} after" \
-                                       f" {time_diff} {ZTZ_SEPARATOR}"
+                    out_str += articulate_data(df, row, col_name)
                 out_str = out_str[:-len(ZTZ_SEPARATOR)] + "\n"
             out_f.write(out_str)
 
